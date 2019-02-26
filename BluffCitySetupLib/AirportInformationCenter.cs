8c6be9029ff86c729f9569d9b29ff6ec92ceb4a2
MathDodo <mathiasdodo@hotmail.com>
2019-02-26 16:58:54 +0100

    - airport informaiton center

---
+++ BluffCitySetupLib/AirportInformationCenter.cs
@@ -0,0 +1,21 @@
+﻿using Data;
+using Setup.Data;
+using System.Collections.Generic;
+
+namespace BluffCitySetupLib
+{
+    public abstract class AirportInformationCenter : MessageQueueAccessor
+    {
+        protected List<AirportInformationCenterData> _centerData;
+
+        public AirportInformationCenter()
+        {
+            _centerData = new List<AirportInformationCenterData>();
+        }
+
+        public void AddCenterData(AirportInformationCenterData data)
+        {
+            _centerData.Add(data);
+        }
+    }
+}
