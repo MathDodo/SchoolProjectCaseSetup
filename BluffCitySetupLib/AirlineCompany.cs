0878162446944d7778cfd2e7c5741dc8c2123729
MathDodo <mathiasdodo@hotmail.com>
2019-02-26 16:53:28 +0100

    - last setup piece for now

--- BluffCitySetupLib/AirlineCompany.cs
+++ BluffCitySetupLib/AirlineCompany.cs
@@ -1,14 +1,24 @@
 ﻿using Data;
+using Setup.Data;
+using System.Collections.Generic;
 
 namespace Setup
 {
     public abstract class AirlineCompany : MessageQueueAccessor
     {
+        protected List<AirlineCompanyData> _flightsInformation;
+
         public string CompanyName { get; private set; }
 
         public AirlineCompany(string name)
         {
             CompanyName = name;
+            _flightsInformation = new List<AirlineCompanyData>();
+        }
+
+        public void AddFlight(AirlineCompanyData flightData)
+        {
+            _flightsInformation.Add(flightData);
         }
     }
 }
